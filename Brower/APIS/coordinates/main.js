409fc17192caea0fe36b16e6bed20311ea4c2ffd
vi2920va <vi2920va@gmail.com>
2021-06-01 19:00:45 +0900

    add shopping list

--- Brower/APIS/coordinates/main.js
+++ Brower/APIS/coordinates/main.js
@@ -1,16 +1,31 @@
-const horizontal = document.querySelector(".horizontal");
-const vertical = document.querySelector(".vertical");
-const target = document.querySelector(".target");
-const tag = document.querySelector(".tag");
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
 
-document.addEventListener("mousemove", event => {
-  const x = event.clientX;
-  const y = event.clientY;
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.innerHTML = `${x}px, ${y}px`;
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', event => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+
+    // target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+
+    // tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
