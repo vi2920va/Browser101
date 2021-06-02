27fb136fd61f8507c5a95a5c0208cd751c60e569
vi2920va <vi2920va@gmail.com>
2021-06-02 20:36:33 +0900

    dom event

--- Brower/APIS/shoppoing/main.js
+++ Brower/APIS/shoppoing/main.js
@@ -7,6 +7,10 @@ const addBtn = document.querySelector('.add__btn');
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받기.
   const value = input.value;
+  if (value === '') {
+    input.focus();
+    return;
+  }
 
   // 2. 새로운 아이템을 생성(텍스트 + 삭제 버튼)
   const item = createItem(value);
@@ -14,7 +18,10 @@ function onAdd() {
   // 3. items 컨테이너안에 새로 만든 아이템을 추가.
   items.appendChild(item);
 
-  // 4. input 초기화
+  // 4. 새로 추가된 item 스크롤링
+  item.scrollIntoView({ block: 'center' });
+
+  // 5. input 초기화
   input.value = '';
   input.focus();
 }
@@ -49,4 +56,10 @@ function createItem(value) {
 addBtn.addEventListener('click', e => {
   e.preventDefault();
   onAdd();
+});
+
+input.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    onAdd();
+  }
 });
