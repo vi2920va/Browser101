1903c5057d521317e9128b0027765b6fe922d78c
vi2920va <vi2920va@gmail.com>
2021-06-22 11:14:57 +0900

    random placement of carrots and bugs

---
+++ Brower/APIS/carrot/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const CARROT_SIZE = 80;
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+
+function initGame() {
+  // 당근과 벌레를 생성.
+  addItem('carrot', 5, './img/carrot.png');
+  addItem('bug', 5, './img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.top = `${y}px`;
+    item.style.left = `${x}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+initGame();
